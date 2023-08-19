40ea4d7096866fe1892cd6335a90b0c57371a8ad
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-08-19 11:43:23 +0100

    sliding window

--- dsa.js
+++ dsa.js
@@ -53,3 +53,19 @@ function countUniqueValues(arr) {
   }
   return i + 1;
 }
+
+// sliding window
+function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum
+    for(let i = num ; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math. max(maxSum, tempSum);
+    }
+    return maxSum;
+}
