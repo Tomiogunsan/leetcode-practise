4d43b40960ca570ad8c0e37fd6473ab46d90585e
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-09-18 16:34:11 +0100

    hash table

--- hash.js
+++ hash.js
@@ -1,11 +1,10 @@
-const names = ["Mary", "John", "Emma"] 
+const names = ["Mary", "John", "Emma"];
 const heights = [180, 165, 170];
 var sortPeople = function (names, heights) {
-let obj = names.map((name, i) => ({name: names[i], hgt:heights[i]}))
-obj.sort((a,b) => b.hgt - a.hgt)
-console.log(obj)
- return obj.map(name => name.name)
-};
-console.log(heights.sort((a,b) => b-a))
+  let obj = names.map((name, i) => ({ name: names[i], hgt: heights[i] }));
+  obj.sort((a, b) => b.hgt - a.hgt);
 
-console.log(sortPeople(names, heights))
+  return obj.map((name) => name.name);
+};
+
+console.log(sortPeople(names, heights));
