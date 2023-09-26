d2a276348fd6c58caa27934cf05d29d97eff1ed8
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-09-26 16:41:46 +0100

    dsa

--- dsa.js
+++ dsa.js
@@ -176,3 +176,22 @@ function binarySearch(arr, elem){
 }
 
 console.log(binarySearch([2,5,6,9,13,15,28,30], 5))
+
+function naiveSearch(long, short){
+  let count = 0;
+  for(let i = 0; i < long.length; i++){
+    for(let j = 0; j < short.length; j++){
+    console.log(j, i + j)
+    if(short[j] !== long[i + j]){
+      console.log("break")
+      break;
+    }
+    if(j === short.length - 1){
+      count++
+    }
+    }
+  }
+  return count
+}
+
+naiveSearch("lorie loled", "lol")
