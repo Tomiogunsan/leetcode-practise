b146cd8d6de903304b705b291b49339861607d13
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-09-26 17:00:40 +0100

    binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,20 +1,16 @@
- const nums = [1, 2, 5, 2, 3]; 
- const target = 2;
+const nums = [1, 2, 5, 2, 3];
+const target = 2;
 
 var targetIndices = function (nums, target) {
-    let res = []
-    const sortNums = nums.sort((a,b) => a-b)
-    console.log(sortNums)
-    sortNums.map((val, i) => {
-        if(val === target){
-            res.push(i)
-            console.log(res)
-            
-        }
-        
-    })
-    return res;
-};
+  let res = [];
+  const sortNums = nums.sort((a, b) => a - b);
 
+  sortNums.map((val, i) => {
+    if (val === target) {
+      res.push(i);
+    }
+  });
+  return res;
+};
 
 console.log(targetIndices(nums, target));
