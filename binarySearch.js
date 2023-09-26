2eeb35591db1d7a44990f9e47643791cce8eb9f9
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-09-26 17:39:52 +0100

    binary search

--- binarySearch.js
+++ binarySearch.js
@@ -14,3 +14,31 @@ var targetIndices = function (nums, target) {
 };
 
 console.log(targetIndices(nums, target));
+
+const grid = [
+  [4, 3, 2, -1],
+  [3, 2, 1, -1],
+  [1, 1, -1, -2],
+  [-1, -1, -2, -3],
+];
+var countNegatives = function (grid) {
+  let count = 0;
+   const res = grid.map((item) => item.sort((a,b) => a - b))
+   console.log(res)
+  res.map((val) => {
+    console.log(val)
+    val.map((element) => {
+        console.log(element)
+        if(element <= -1){
+            console.log(element)
+            count++
+            console.log(count)
+        }
+    })
+  })
+  console.log(count)
+
+return count
+};
+
+console.log(countNegatives(grid));
