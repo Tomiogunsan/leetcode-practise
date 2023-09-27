b001dfbb572c93f07b4e52093d0eb73c112b7e92
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-09-27 18:07:39 +0100

    insertion sort

--- dsa.js
+++ dsa.js
@@ -232,3 +232,17 @@ function selectionSort(arr){
 }
 
 console.log(selectionSort([0,2,43,5,75,18,21]));
+
+// insertion sort
+function insertionSort(arr){
+  for(var i = 1; i < arr.length; i++){
+    var currentVal = arr[i];
+    for(var j = i - 1; j>=0 && arr[j] > currentVal; j--){
+      arr[j+1] = arr[j]
+    }
+    arr[j+1] = currentVal
+  }
+  return arr;
+}
+
+console.log(insertionSort([2,1,9,76,4]))
