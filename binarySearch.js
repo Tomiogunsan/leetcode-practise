280b80c27f2d985f83c943687fa9661b18864e5b
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-04 12:45:06 +0100

    binary search

--- binarySearch.js
+++ binarySearch.js
@@ -144,25 +144,23 @@ var maximumCount = function (a) {
 
 console.log(maximumCount(a));
 
-const arr1 = [4, 5, 8];  
-const arr2 = [10, 9, 1, 8] 
+const arr1 = [4, 5, 8];
+const arr2 = [10, 9, 1, 8];
 const d = 2;
 var findTheDistanceValue = function (arr1, arr2, d) {
-  let res = 0
-for(let i = 0; i < arr1.length; i++){
-  console.log(arr1[i])
-  let count = 0
-  for( let j = 0; j < arr2.length; j++){
-    if(Math.abs(arr1[i]-arr2[j]) <= d) {
-      count=1;
-      break;
+  let res = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    let count = 0;
+    for (let j = 0; j < arr2.length; j++) {
+      if (Math.abs(arr1[i] - arr2[j]) <= d) {
+        count = 1;
+        break;
+      }
     }
-    
+    if (count === 0) res++;
   }
-  if(count === 0)res++
-}
-console.log(res)
-return res
+
+  return res;
 };
 
 console.log(findTheDistanceValue(arr1, arr2, d));
