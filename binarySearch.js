f1c9b303dc466ca5765b70d199188bf59e0b042d
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-07 18:24:04 +0100

    cartesianProduct

--- binarySearch.js
+++ binarySearch.js
@@ -254,3 +254,12 @@ return res
   }
 
   console.log(cartesianProduct(arrA, arrB))
+
+  // fibonacci
+  function climbingStaircase(n){
+    const noOfWays = [1,2]
+    for(let i = 2; i <=n; i++){
+      noOfWays[i] = noOfWays[i - 1] + noOfWays[i - 2]
+    }
+    return noOfWays[n - 1 ]
+  }
