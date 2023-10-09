d153d6e3a73985cb7b552437095dfceced9f92ad
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-09 14:32:10 +0100

    math: isPrime

--- dsa.js
+++ dsa.js
@@ -342,3 +342,18 @@ function factorial(n){
 }
 
 console.log(factorial(3))
+
+// prime number
+function isPrime(n){
+  if(n < 2){
+    return false
+  }
+  for(let i = 2; i < n; i++){
+    if( n % i === 0){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(isPrime(8))
