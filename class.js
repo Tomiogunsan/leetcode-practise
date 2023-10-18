dee90521a3429b50324e010f1ac59bce82ed94f7
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-18 19:12:48 +0100

    class

---
+++ class.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(firstName, lastName, year){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.grade = year
+    }
+}
+
+let student = new Student('eilly', 'grd', 6)
+console.log(student)
