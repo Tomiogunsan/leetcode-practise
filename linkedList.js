ae5c89661bfdd595bc00e210674c5b25037f7af5
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-20 19:15:44 +0100

    linked list: unshift

--- linkedList.js
+++ linkedList.js
@@ -51,5 +51,17 @@ class SinglyLinkedList{
         }
         return currentHead
     }
+    // unshift
+    unshift(val){
+        var newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail= this.head
+        }
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
 }
 let list = new SinglyLinkedList()
