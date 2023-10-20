058af0b97ef3edba7c96ce5426c0fd42a94b6da6
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-20 19:32:52 +0100

    linked list: get

--- linkedList.js
+++ linkedList.js
@@ -1,67 +1,79 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class SinglyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    push(val){
-        var newNode = new Node(val);
-        if(!this.head){
-            this.head = newNode
-            this.tail = this.head
-        }else{
-            this.tail.next = newNode
-            this.tail = newNode
-        }
-        this.length++
-        return this;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
-    pop(){
-        if(!this.head)return undefined
-        let current = this.head
-        let newTail = current;
-        while(current.next){
-            newTail = current;
-            current = current.next;
-        }
-        this.tail = newTail
-        this.tail.next = null
-        this.length--
-        if(this.length === 0){
-            this.head = null
-            this.tail = null
-        }
-        return current
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
     }
-    // shift
-    shift(){
-        if(!this.head === 0)return undefined
-        let currentHead = this.head
-        this.head = currentHead.next
-        this.length--
-        if(this.length === 0){
-            this.tail = null;
-        }
-        return currentHead
+    return current;
+  }
+  // shift
+  shift() {
+    if (!this.head === 0) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
     }
-    // unshift
-    unshift(val){
-        var newNode = new Node(val)
-        if(!this.head){
-            this.head = newNode
-            this.tail= this.head
-        }
-        newNode.next = this.head
-        this.head = newNode
-        this.length++
-        return this
+    return currentHead;
+  }
+  // unshift
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
     }
+    this.length++;
+    return this;
+  }
+//   get
+get(index){
+    if(index < 0 || index >= this.length) return null
+    let counter = 0;
+    let current = this.head
+    while(counter !== index){
+        current = current.next
+        counter++
+    }
+    return current
 }
-let list = new SinglyLinkedList()
+}
+let list = new SinglyLinkedList();
