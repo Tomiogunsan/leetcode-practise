b892aa2f93d79571026f4fb5bf4a935acb5cad82
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-21 20:11:19 +0100

    linked list: remove

--- linkedList.js
+++ linkedList.js
@@ -97,5 +97,16 @@ newNode.next = temp
 this.length++
 return true; 
 }
+// remove
+remove(index){
+  if(index < 0 || index >= this.length) return undefined
+  if(index === 0)return this.shift()
+  if(index === this.length - 1)return this.pop()
+  var previousNode = this.get(index - 1)
+var removed = previousNode.next
+previousNode.next = removed.next
+this.length--
+return removed
+}
 }
 let list = new SinglyLinkedList();
