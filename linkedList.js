65a54c6bf603b6f564a0614895b0760f694c3066
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-10-23 20:33:10 +0100

    linked list

--- linkedList.js
+++ linkedList.js
@@ -193,6 +193,25 @@ class LinkedList {
 
     }
   }
+  removeFrom(index){
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    let removedNode
+    if(index === 0){
+      removedNode = this.head
+      this.head = this.head.next
+    }else{
+      let prev = this.head
+      for(let i = 0; i < index - 1; i++){
+        prev = prev.next
+      }
+      removedNode = prev.next
+      prev.next = removedNode.next
+    }
+    this.size--
+    return removedNode.value
+  }
   print(){
     if(this.isEmpty()){
       console.log('List is empty')
