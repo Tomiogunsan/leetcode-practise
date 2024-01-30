92b8b16854a2c885283cb6558b45a50758514b47
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-01-30 12:44:53 +0100

    solve problem

--- dsa.js
+++ dsa.js
@@ -2,42 +2,33 @@ const username = ["bees", "superhero", "ace"];
 
 function canSwapToSmaller(username) {
   // Convert the string to an array of characters
-    
-  }
-
-
-function result () {
-    return username.map(canSwapToSmaller)
 }
 
+function result() {
+  return username.map(canSwapToSmaller);
+}
 
 console.log(canSwapToSmaller(username));
-console.log(result())
+console.log(result());
 
 const haystack = "sadbutsad";
 const needle = "sad";
 var strStr = function (haystack, needle) {
-   return haystack.indexOf(needle);
+  return haystack.indexOf(needle);
 };
 
 console.log(strStr(haystack, needle));
 
 const strs = ["flower", "flow", "flight"];
 var longestCommonPrefix = function (strs) {
-    if (strs.length === 0) {
-      return "";
-    }
-    let prefix = strs[0];
-    console.log(prefix)
-    for(let word of strs){
-        console.log(word.indexOf(prefix) !== 0)
-        while(word.indexOf(prefix) !== 0){
-            prefix = prefix.slice(0,  -1);
-            console.log(prefix)
-            if (prefix === "") return "";
-        }
-    }
-    return prefix
+  let output = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    console.log(strs[0][i]);
+    if (strs.every((item) => item[i] === strs[0][i])) {
+      output += strs[0][i];
+    }else break
+  }
+  return output;
 };
 
 console.log(longestCommonPrefix(strs));
