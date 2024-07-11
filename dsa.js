ab046262e6a5e12450b272dcc553c7118ba58ec0
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-07-11 10:40:05 +0100

    ch: array methods

--- dsa.js
+++ dsa.js
@@ -158,4 +158,20 @@ var countGoodTriplets = function (ar, ab, ba, c) {
   return count;
 };
 
+const nums = [2, 7, 11, 15];
+const target = 9;
+var twoSum = function (nums, target) {
+  const map = new Map();
 
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+};
+
+console.log(twoSum(nums, target));
