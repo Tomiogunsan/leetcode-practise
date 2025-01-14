fe3495cb02717cd431bb889444f1fcf3bf6eecd6
Tomi Ogunsan <tomiafolayan@gmail.com>
2025-01-14 11:26:42 +0100

    find the lowest value

--- fibonacci.js
+++ fibonacci.js
@@ -1,73 +1,70 @@
-let prev2 = 0
-let prev1 = 1
+let prev2 = 0;
+let prev1 = 1;
 
 const fibo = () => {
-    for (let i = 0; i < 18; i++) {
-      let newFibo = prev1 + prev2;
-      console.log(newFibo);
-      prev2 = prev1;
-      prev1 = newFibo;
-    }
-}
+  for (let i = 0; i < 18; i++) {
+    let newFibo = prev1 + prev2;
+    console.log(newFibo);
+    prev2 = prev1;
+    prev1 = newFibo;
+  }
+};
 
-console.log(fibo())
-
+console.log(fibo());
 
 const my_array = [7, 12, 9, 4, 11];
 
 const findTheLowestValue = () => {
-    let minVal = my_array[0];
-    for(i = 0; i < my_array.length; i++){
-        if(my_array[i] < minVal){
-            minVal = my_array[i];
-        }
-       
+  let minVal = my_array[0];
+  for (i = 0; i < my_array.length; i++) {
+    if (my_array[i] < minVal) {
+      minVal = my_array[i];
     }
-     return minVal;
-}
+  }
+  return minVal;
+};
 
-console.log(findTheLowestValue())
+console.log(findTheLowestValue());
 
-
-let myArray = [64, 34, 25, 12, 22, 11, 90]; 
+let myArray = [64, 34, 25, 12, 22, 11, 90];
 
 // bubble sort
 const findTheLargestValue = () => {
-    for( i = 0; i < myArray.length; i++){
-          let swapped = false;
-        for(j = 0; j < myArray.length - 1; j++){
-            if(myArray[j] > myArray[j + 1]){
-                let temp = myArray[j];
-                myArray[j] = myArray[j + 1];
-                myArray[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        if (!swapped) {
-        break;
+  for (i = 0; i < myArray.length; i++) {
+    let swapped = false;
+    for (j = 0; j < myArray.length - 1; j++) {
+      if (myArray[j] > myArray[j + 1]) {
+        let temp = myArray[j];
+        myArray[j] = myArray[j + 1];
+        myArray[j + 1] = temp;
+        swapped = true;
+      }
     }
+    if (!swapped) {
+      break;
     }
-    return myArray
-}
+  }
+  return myArray;
+};
 
-console.log(findTheLargestValue())
+console.log(findTheLargestValue());
 
 const findTheLowest = () => {
-    for( i = 0; i < myArray.length; i++){
-          let swapped = false;
-        for(j = 0; j < myArray.length - 1; j++){
-            if(myArray[j] < myArray[j + 1]){
-                let temp = myArray[j];
-                myArray[j] = myArray[j + 1];
-                myArray[j + 1] = temp;
-                swapped = true;
-            }
-        }
-        if (!swapped) {
-        break;
+  for (i = 0; i < myArray.length; i++) {
+    let minIndex = i
+    for (j = i + 1; j < myArray.length ; j++) {
+      if (myArray[j] < myArray[minIndex]) {
+        minIndex = j
+      }
     }
+    if (minIndex !== i) {
+     let temp = myArray[minIndex]
+     console.log(temp)
+     myArray[minIndex] = myArray[i]
+     myArray[i] = temp
     }
-    return myArray
-}
+  }
+  return myArray;
+};
 
-console.log(findTheLowest())
+console.log(findTheLowest());
